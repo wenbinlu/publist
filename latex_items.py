39b21d73fdf71bba09867962e7968ce876a7f1c8
wenbinlu <windbinderue@gmail.com>
2025-08-19 14:23:04 -0700

    fixed a bug that prevents finding year, month, etc. uniquely

--- latex_items.py
+++ latex_items.py
@@ -1,3 +1,12 @@
+import numpy as np
+
+t_range_mark = False   # whether to mark pub in the following range (yr+mon)
+tpub_max = 202306
+tpub_min = 202207
+
+tmax_cut = False       # whether to cut off at tpub_max
+chronological = False   # False for inverse chronological order
+
 fdir = './'
 fname = 'export-bibtex.bib'
 list_savename = ['py_list_submitted.tex', 'py_list_123_published.tex',
@@ -10,12 +19,15 @@ Nauthor_crit = 4   # critical number of authors for folding
 journal_dict = {'mnras': 'MNRAS', 'apj': 'ApJ', 'apjl': 'ApJL',
                 'apjs': 'ApJS', 'apss': 'Ap\\&SS', 'aa': 'A\\&A',
                 'aap': 'A\\&A', 'aj': 'AJ', 'aara': 'ARA\\&A',
-                '\\nat': 'Nature', 'pasj': 'PASJ',
+                'nat': 'Nature', 'pasj': 'PASJ',
                 'ssr': 'Space Sci. Rev.', 'GCN': 'GCN Circ',
                 'PRD': 'Phys. Rev. D', 'prd': 'Phys. Rev. D',
                 'PRL': 'Phys. Rev. L', 'prl': 'Phys. Rev. L',
                 'procspie': 'Proc. SPIE', 'nar': 'New Astronomy Reviews',
                 'arXiv e-prints': 'submitted'}
+month_dict = {'jan': 1, 'feb': 2, 'mar': 3, 'apr': 4, 'may': 5,
+              'jun': 6, 'jul': 7, 'aug': 8, 'sep': 9, 'oct': 10,
+              'nov': 11, 'dec': 12}
 
 
 # obtain the info within the bracket for 'target = {...}'
@@ -56,8 +68,9 @@ def write_tail(fsave):
 
 
 def write_item(fsave, my_last, my_first, i_paper, author_last,
-              author_first, title, journal,
-              year, volume, number, eprint, adsurl):
+               author_first, title, journal,
+               year, month, volume, number, eprint, adsurl,
+               mark, refereed):
     fsave.write('\n')
     fsave.write('\\vspace{-0.1cm}\n')
     authors = ''
@@ -77,6 +90,7 @@ def write_item(fsave, my_last, my_first, i_paper, author_last,
                 authors += 'et al., '
             break
     it_title = '{\\it ' + title + '}, '
+    title = '{' + title + '}, '
     if journal == 'submitted':
         volume = ''
         number = ''
@@ -87,17 +101,30 @@ def write_item(fsave, my_last, my_first, i_paper, author_last,
             volume += ', '
         if number != '':
             number += ', '
+    it_journal = '{\\it ' + journal + '}, '
     journal = journal + ', '
     # print(i_paper, authors, year, it_title, journal,
     #       volume, number, eprint, adsurl)
-    fsave.write('\\item[%d.]{' % i_paper
-                + authors
-                + year + ', '
-                + it_title
-                + journal + volume + number
-                + eprint
-                + '\\href{' + adsurl + '}{\\underline{PDF}}'
+    c_number = '%d' % i_paper
+    if not refereed:  # not a refereed publication
+        c_number = '*' + c_number
+    if t_range_mark and mark:
+        fsave.write('\\item[\\textbf{' + c_number + '.}]{')
+    else:
+        fsave.write('\\item[' + c_number + '.]{')
+    fsave.write(authors + title + it_journal
+                + volume + number + eprint
+                + '(' + year + ')'
+                + '(' + month + ')'
+                + ' \\href{' + adsurl + '}{\\underline{PDF}}'
                 + '}\n')
+    #fsave.write(authors
+    #            + year + ', '
+    #            + it_title
+    #            + journal + volume + number
+    #            + eprint
+    #            + '\\href{' + adsurl + '}{\\underline{PDF}}'
+    #            + '}\n')
 
 
 # go through each line to find the number of papers
@@ -111,8 +138,9 @@ for i in range(Nlines):
 i_paper = 0
 list_i_author = []
 list_author_last, list_author_first, list_title = [], [], []
-list_journal, list_year, list_volume = [], [], []
+list_journal, list_year, list_month, list_volume = [], [], [], []
 list_number, list_eprint, list_adsurl = [], [], []
+list_refereed = []
 i = 0
 while i < Nlines:
     new_line = data[i]
@@ -165,15 +193,22 @@ while i < Nlines:
                 j += 1
         # print(author_last)
         # print(author_first)
-        title = find_in_brackets(lines, 'title')
-        journal = find_in_brackets(lines, 'journal').replace('\\', '')
+        title = find_in_brackets(lines, 'title = ')
+        booktitle = find_in_brackets(lines, 'booktitle = ')
+        journal = find_in_brackets(lines, 'journal = ').replace('\\', '')
         if journal in list(journal_dict.keys()):
             journal = journal_dict[journal]
-        year = find_simple(lines, 'year')
-        volume = find_in_brackets(lines, 'volume')
-        number = find_in_brackets(lines, 'number')
-        adsurl = find_in_brackets(lines, 'adsurl')
-        eprint = find_in_brackets(lines, 'eprint')
+        if journal == '' and booktitle != '':  # this is a book
+            list_refereed += [False]  # not refereed
+            journal = booktitle
+        else:
+            list_refereed += [True]
+        year = find_simple(lines, 'year = ')
+        month = find_simple(lines, 'month = ')
+        volume = find_in_brackets(lines, 'volume = ')
+        number = find_in_brackets(lines, 'number = ')
+        adsurl = find_in_brackets(lines, 'adsurl = ')
+        eprint = find_in_brackets(lines, 'eprint = ')
         i_author = 0
         while i_author < len(author_last):
             if author_last[i_author] == my_last and author_first[i_author][0] == my_first[0]:
@@ -185,6 +220,7 @@ while i < Nlines:
         list_title += [title]
         list_journal += [journal]
         list_year += [year]
+        list_month += [month]
         list_volume += [volume]
         list_number += [number]
         list_eprint += [eprint]
@@ -193,6 +229,27 @@ while i < Nlines:
 print('number of first- and second-author papers: %d and %d'
       % (list_i_author.count(1), list_i_author.count(2)))
 
+# sort the lists according to publication time (or submission time if not published)
+list_pub_time = []
+list_mark_or_not = []
+for i in range(Npapers):
+    t_year = '%4d' % int(list_year[i])
+    if list_month[i] == '':  # month information is not available
+        t_month = '12'
+    else:
+        t_month = '%02d' % int(month_dict[list_month[i]])
+    t_combined = int(t_year + t_month)
+    if tpub_min<= t_combined <= tpub_max:
+        list_mark_or_not += [True]
+    else:
+        list_mark_or_not += [False]
+    list_pub_time += [t_combined]
+    
+if chronological:
+    index_list = np.argsort(list_pub_time)   # chronological order
+else:
+    index_list = np.argsort(list_pub_time)[::-1]   # inverse chronological order
+    
 # count the number of papers in each category
 Nsub, N123, Nn = 0, 0, 0
 for i in range(Npapers):
@@ -207,25 +264,43 @@ list_fsave = [open(fdir+savename, 'w') for savename in list_savename]
 [write_head(fsave) for fsave in list_fsave]
 
 c1, c2, c3, c4 = 0, 0, 0, 0
-for i in range(Npapers):
+#for i in range(Npapers):
+for i in index_list:
+    if tmax_cut:  # do not include publications later than tpub_max
+        if list_pub_time[i] > tpub_max:
+            continue
+    
     if list_journal[i] == 'submitted':
         fsave = list_fsave[0]
-        c = Nsub - c1
+        if chronological:
+            c = c1 + 1
+        else:
+            c = Nsub - c1
         c1 += 1
     elif int(list_i_author[i]) <= 3:
         fsave = list_fsave[1]
-        c = N123 - c2
+        if chronological:
+            c = c2 + 1
+        else:
+            c = N123 - c2
         c2 += 1
     else:
         fsave = list_fsave[2]
-        c = Nn - c3
+        if chronological:
+            c = c3 + 1
+        else:
+            c = Nn - c3
         c3 += 1
     args = list_author_last[i], list_author_first[i], list_title[i],\
-           list_journal[i], list_year[i], list_volume[i],\
-           list_number[i], list_eprint[i], list_adsurl[i]
+           list_journal[i], list_year[i], list_month[i], list_volume[i],\
+           list_number[i], list_eprint[i], list_adsurl[i], \
+           list_mark_or_not[i], list_refereed[i]
     write_item(fsave, my_last, my_first, c, *args)
     if list_journal[i] != 'submitted':
-        c = Npapers-Nsub-c4
+        if chronological:
+            c = c4 + 1
+        else:
+            c = Npapers-Nsub-c4
         c4 += 1
         write_item(list_fsave[3], my_last, my_first, c, *args)
 
